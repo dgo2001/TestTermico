4a399a55a31b879bf58a52dbefcb1fadd7c785a9
dgo2001 <d.olias2001@gmail.com>
2024-05-26 13:34:00 +0200

    Ejercicio burguers con simbolico

--- burgers.py
+++ burgers.py
@@ -1,3 +1,5 @@
+### Resolucion de la ecuacion de Burgers en 1D con condiciones de contorno periodicas
+
 import numpy as np
 import sympy as sp 
 
@@ -25,3 +27,13 @@ un = np.empty(nx)
 t = 0
 
 u = np.asarray([ufunc(t, x0, nu) for x0 in x]) # Condiciones iniciales de velocidad, funcion sawtooth
+
+for n in range(nt):
+    un = u.copy()
+    for i in range(1, nx-1):
+        u[i] = un[i] - un[i] * dt / dx *(un[i] - un[i-1]) + nu * dt / dx**2 *\
+                (un[i+1] - 2 * un[i] + un[i-1])
+        
+    u[0] = un[0] - un[0] * dt / dx * (un[0] - un[-2]) + nu * dt / dx**2 *\
+                (un[1] - 2 * un[0] + un[-2])
+    u[-1] = u[0]
