ba028720c2c568a339ee00a2610abeef22177c92
dgo2001 <d.olias2001@gmail.com>
2024-05-26 13:16:41 +0200

    Ejercicio burguers con simbolico

---
+++ burgers.py
@@ -0,0 +1,11 @@
+import numpy as np
+import sympy as sp 
+
+from sympy import init_printing
+init_printing(use_latex=True)
+
+x, nu, t = sympy.symbols('x nu t')
+phi=(sympy.exp(-(x-4*t)**2/(4*nu*(t+1)))+
+       sympy.exp(-(x-4*t-2*sympy.pi)**2/(4*nu*(t+1))))
+phi
+
